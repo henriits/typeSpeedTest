5db935dc302d94475d8bcaf3716b66de9f2e0886
henriits <henritsarents@gmail.com>
2024-02-18 20:31:18 +0200

    .gitignore

---
+++ requrements.md
@@ -0,0 +1,3 @@
+instead of adding text directly to html
+
+need to save text in list, and split each letter, and add each letter seperately to html
--- textFromApi.js
+++ textFromApi.js
@@ -10,15 +10,25 @@ async function getRandomAuthor() {
 async function getRandomLinesFromAuthor(author) {
     let response = await fetch(`https://poetrydb.org/author/${author}/lines`);
     let data = await response.json();
-    let lines = data[Math.floor(Math.random() * data.length)];
+    let lines = data[Math.floor(Math.random() * data.length)].lines;
     return lines;
 }
 
-getRandomAuthor()
-    .then(async randomAuthor => {
-    //console.log("Random Author:", randomAuthor);
+async function displayRandomLetters() {
+    let randomAuthor = await getRandomAuthor();
     let randomLines = await getRandomLinesFromAuthor(randomAuthor);
-    console.log("Random Lines:", randomLines);
-    let p = document.getElementById("lines-go-here")
-    p.innerHTML = randomLines.lines
-});
+
+    let container = document.getElementById("lines-go-here");
+    container.innerHTML = ""; // Clear previous content
+
+    for (let i = 0; i < randomLines.length; i++) {
+        let line = randomLines[i];
+        for (let j = 0; j < line.length; j++) {
+            let letterDiv = document.createElement("div");
+            letterDiv.textContent = line[j];
+            container.appendChild(letterDiv);
+        }
+    }
+}
+
+displayRandomLetters();
