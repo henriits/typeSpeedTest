bdded1f7c4a6e020e34b16bd609fa0a786bf5965
henriits <henritsarents@gmail.com>
2024-02-18 19:03:07 +0200

    get data random lines from random author on api

---
+++ main.js
@@ -0,0 +1,21 @@
+async function getRandomAuthor() {
+    let response = await fetch("https://poetrydb.org/author");
+    let data = await response.json();
+    let authors = data.authors;
+    let randomIndex = Math.floor(Math.random() * authors.length);
+    let randomAuthor = authors[randomIndex];
+    return randomAuthor;
+}
+
+async function getRandomLinesFromAuthor(author) {
+    let response = await fetch(`https://poetrydb.org/author/${author}/lines`);
+    let data = await response.json();
+    let lines = data[Math.floor(Math.random() * data.length)];
+    return lines;
+}
+
+getRandomAuthor().then(async randomAuthor => {
+    //console.log("Random Author:", randomAuthor);
+    let randomLines = await getRandomLinesFromAuthor(randomAuthor);
+    console.log("Random Lines:", randomLines);
+});
