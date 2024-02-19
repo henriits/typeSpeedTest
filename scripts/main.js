54cbff63fafb062e440f8478197306acc1a142f1
henriits <henritsarents@gmail.com>
2024-02-19 17:09:34 +0200

    add simple divs for titles and for function

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 import { displayRandomLetters } from "./textFromApi.js";
 
 
-async function displayRandom() {
-    let text = await displayRandomLetters(15, 20); // Display min lines, max lines
-    return text
-}
 
-displayRandom(); // Call the async function to start the process
+document.addEventListener('DOMContentLoaded', async () => {
+
+    async function displayRandom() {
+        let text = await displayRandomLetters(15, 20); // Display min lines, max lines
+        return text;
+    }
+
+    await displayRandom(); // Call the async function to start the process
+});
