d38f2b552ed03ca75f7b431ed07d6513c4b3b1b5
henriits <henritsarents@gmail.com>
2024-02-21 20:48:37 +0200

    correct and false classes added to span, now text changes color

--- scripts/input.js
+++ scripts/input.js
@@ -15,8 +15,21 @@ export function InputLetters() {
         errors = 0;
 
         let randomTextSpan = document.querySelectorAll(".letter"); // Target spans with the added class
-        
         console.log(randomTextSpan)
+        randomTextSpan.forEach((char, index) => {
+            let typed = textArray[index];
+            if (typed == null) {
+                char.classList.remove("correct");
+                char.classList.remove("false"); // Remove both classes when there's no typed character
+            } else if (typed === char.innerText) {
+                char.classList.add("correct");
+                char.classList.remove("false"); // Remove "false" class when typed character matches
+            } else {
+                char.classList.remove("correct"); // Remove "correct" class if typed character doesn't match
+                char.classList.add("false");
+            }
+        });
+
 
     });
 }
